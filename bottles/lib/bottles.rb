157080b3c4180e0d4632751a00d680f693fbaa1b
Sandi Metz <sandi@sandimetz.com>
2019-11-05 15:14:44 -0800

    extract BottleVerse

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,4 +1,9 @@
 class Bottles
+  attr_reader :verse_template
+  def initialize(verse_template: BottleVerse)
+    @verse_template = verse_template
+  end
+
   def song
     verses(99, 0)
   end
@@ -8,8 +13,21 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.for(number)
+    verse_template.lyrics(number)
+  end
+end
 
+class BottleVerse
+  def self.lyrics(number)
+    new(BottleNumber.for(number)).lyrics
+  end
+
+  attr_reader :bottle_number
+  def initialize(bottle_number)
+    @bottle_number = bottle_number
+  end
+
+  def lyrics
     "#{bottle_number.description} of beer on the wall, ".capitalize +
     "#{bottle_number.description} of beer.\n" +
     "#{bottle_number.action}, " +
